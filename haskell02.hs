720c4097f816c6ab2f1df6b3805a8b40451e24d2
Bianca Sabrina Sabrina <>
2021-05-26 22:37:45 +0000

    inicio da atividade 2

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,24 @@
 -- Prática 02 de Haskell
--- Nome: 
+-- Nome: Bianca Sabrina Bublitz
+
+verificaFebre :: Float -> Bool
+verificaFebre x = x > 37.80
+
+-- 01
+comFebre :: [Float] -> [Float]
+comFebre x = filter verificaFebre x
+
+-- 02
+comFebre' :: [Float] -> [Float]
+comFebre' x = filter (\x -> x > 37.80) x
+
+-- 03
+itemize :: [String] -> [String]
+itemize x = map (\x -> "<li>" ++ x ++ "</li>") x
+
+calculaArea :: Float -> Bool
+calculaArea x = pi * (x^2)
+
+-- 04
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles area raios = filter (\x -> x > area)(map 
