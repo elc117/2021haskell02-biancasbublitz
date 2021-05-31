ebb5e6a39d9c43976d03b416b5fd1ee76d711e46
Bianca Sabrina Sabrina <>
2021-05-31 17:52:24 +0000

    finalização das atividades da parte 2

--- haskell02.hs
+++ haskell02.hs
@@ -16,9 +16,22 @@ comFebre' x = filter (\x -> x > 37.80) x
 itemize :: [String] -> [String]
 itemize x = map (\x -> "<li>" ++ x ++ "</li>") x
 
-calculaArea :: Float -> Bool
-calculaArea x = pi * (x^2)
-
 -- 04
 bigCircles :: Float -> [Float] -> [Float]
-bigCircles area raios = filter (\x -> x > area)(map 
+bigCircles area raios = [x | x <- raios, (pi * x^2)>area]
+
+-- 05
+quarentena :: [(String,Float)] -> [(String,Float)]
+quarentena temp = filter (\ (_,temp) -> verificaFebre temp) temp
+
+-- 06
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm ano_nasc ano_ref = map (\x -> ano_ref - x) ano_nasc
+
+-- 07
+changeNames :: [String] -> [String]
+changeNames nomes = [if head(x)=='A' then "Super "++ x  else x | x <- nomes]
+
+-- 08
+onlyShorts :: [String] -> [String]
+onlyShorts str = filter (\ x -> length(x)<5) str
